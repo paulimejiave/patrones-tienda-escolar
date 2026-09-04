ec3dd8f0ac94cc6689d14bee82740d645cf9e863
paulimejiave <paulina.mejiave@amigo.edu.co>
2026-09-04 15:10:54 -0500

    Update EntregadoState.ts

--- src/patterns/state/EntregadoState.ts
+++ src/patterns/state/EntregadoState.ts
@@ -1,11 +1,16 @@
 import type { OrderState } from "./OrderState";
+import { DevueltoState } from "./DevueltoState";
+import type { PedidoContext } from "./PedidoContext";
 
-/** PATRÓN: State — Rol: "ConcreteState" (estado final en esta versión) */
+/** PATRÓN: State – Rol: "ConcreteState" */
 export class EntregadoState implements OrderState {
   readonly name = "Entregado";
 
+  constructor(private context: PedidoContext) {}
+
   avanzar(): void {
-    throw new Error("El pedido ya llegó a su estado final: Entregado.");
+    // Al avanzar desde Entregado, pasa al nuevo estado Devuelto
+    this.context.cambiarEstado(new DevueltoState(this.context));
   }
 
   cancelar(): void {
